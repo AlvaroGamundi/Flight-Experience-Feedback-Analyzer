4e21fbedddb2e6f77a580a6cd981880c4eda15fa
AlvaroGamundi <alvaro_gamundi@hotmail.com>
2025-04-26 13:57:36 +0200

    Add files via upload

--- app_sentimientos.py
+++ app_sentimientos.py
@@ -15,8 +15,8 @@ st.set_page_config(
 # Ahora la imagen y el título estarán centrados en la parte superior
 st.image('risk_score.jpg', width=300)  # Tamaño más grande, ajustado a 300px de ancho
 
-# Texto con el nombre del creador debajo de la imagen
-st.markdown("<h4 style='text-align: center; color: #888;'>Created by Álvaro Gamundi</h4>", unsafe_allow_html=True)
+# Texto con el nombre del creador debajo de la imagen, más pequeño y alineado a la izquierda
+st.markdown("<h5 style='color: #888;'>Created by Álvaro Gamundi</h5>", unsafe_allow_html=True)
 
 # Título de la app (más grande y llamativo) justo debajo de la imagen
 st.markdown("<h1 style='text-align: center; color: #5F6368;'>Flight Experience Feedback Analyzer</h1>", unsafe_allow_html=True)
@@ -71,6 +71,7 @@ if st.button("Submit your comment"):
 
         except Exception as e:
             st.error(f"Error: {e}")
+
 
 
 
